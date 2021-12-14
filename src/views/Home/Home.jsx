0e84a12a265c8feb015335225e5771296496f47a
‘Katie <ktschr11@gmail.com>
2021-12-14 14:41:02 -0800

    added context wrapping to Home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,3 +1,18 @@
+import EntryList from '../../components/EntryList/EntryList'
+import Guestbook from '../../components/GuestBook/Guestbook'
+import { EntryProvider } from '../../context/EntryContext'
+
+const Home = () => {
+  return (
+    <EntryProvider>
+      <Guestbook />
+      <EntryList />
+    </EntryProvider>
+  )
+}
+
+export default Home;
+
 // TO DO 
 // will import entrylist and guestbook, as well as entry provider
 // declare a component that returns the guest book and list wrapped in the entryprovider
