b7a1ee4b5860dd775bff6b63ff9bc784e1456fea
‘Katie <ktschr11@gmail.com>
2021-12-13 17:46:30 -0800

    more plans and schemes

---
+++ src/components/GuestBook/Guestbook.jsx
@@ -0,0 +1,15 @@
+// TO DOS
+// import useEntries and useUser
+// destructure state and setState before render
+// the name and the setName, and guestEntry and setGuestEntry will be kept locally
+//will need an update guest name function
+// and a handle submit that updates the guests name in state
+// declare the HTML for the guest name input as a variable, and conditionally show 
+
+export default function Guestbook() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
