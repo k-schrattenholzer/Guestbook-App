eb3d59958155882c15a7ce4a1416473a492294ea
‘Katie <ktschr11@gmail.com>
2021-12-14 14:58:52 -0800

    added Layout view

--- src/views/Layout/Layout.jsx
+++ src/views/Layout/Layout.jsx
@@ -1,3 +1,15 @@
+import Footer from '../../components/layout/Footer'
+import Header from '../../components/layout/Header'
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Header/>
+      <main>{children}</main>
+      <Footer />
+    </div>
+  )
+}
 // TO DOS
 // import header and footer components
 // create layout component that takes in a child object
