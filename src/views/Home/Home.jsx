11978c01f0cc872a80cfdf08dfee93565ed80567
‘Katie <ktschr11@gmail.com>
2021-12-13 17:52:19 -0800

    schemes and strats

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,3 @@
+// TO DO 
+// will import entrylist and guestbook, as well as entry provider
+// declare a component that returns the guest book and list wrapped in the entryprovider
---
+++ src/views/Layout/Layout.jsx
@@ -0,0 +1,4 @@
+// TO DOS
+// import header and footer components
+// create layout component that takes in a child object
+// and returns div/header/main(children)/footer
