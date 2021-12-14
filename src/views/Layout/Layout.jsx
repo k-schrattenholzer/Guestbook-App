2724409e0b2b97e1093314ba5910d05355b1a2ba
‘Katie <ktschr11@gmail.com>
2021-12-14 14:59:18 -0800

    added export

--- src/views/Layout/Layout.jsx
+++ src/views/Layout/Layout.jsx
@@ -10,6 +10,9 @@ const Layout = ({ children }) => {
     </div>
   )
 }
+
+export default Layout;
+
 // TO DOS
 // import header and footer components
 // create layout component that takes in a child object
