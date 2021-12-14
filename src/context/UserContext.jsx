5a463629e0c7ac153e4b5157defda6628fcb74ff
‘Katie <ktschr11@gmail.com>
2021-12-13 17:55:47 -0800

    added userprovider component

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,5 +1,14 @@
+import { useContext, createContext, useState } from 'react'
+
 // Create the Context
+const userContext = createContext();
+
 // Create the UserProvider component
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState('');
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
+}
 // Create custom hook
 
  // `context` will be undefined when using this custom hook
