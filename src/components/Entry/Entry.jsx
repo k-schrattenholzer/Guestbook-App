fa7045e6382a6adf3b82484443bb7e980761c453
‘Katie <ktschr11@gmail.com>
2021-12-13 17:15:03 -0800

    planning

---
+++ src/components/Entry/Entry.jsx
@@ -0,0 +1,8 @@
+
+export default function Entry() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/EntryList/EntryList.jsx
@@ -0,0 +1,11 @@
+// TO DO
+// will import Entry, and useEntries
+//before the return, destructure entries from useEntries
+
+export default function EntryList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
