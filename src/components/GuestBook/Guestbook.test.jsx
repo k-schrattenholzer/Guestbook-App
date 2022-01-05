0ed02562a2491757bcd74c68ce403c8487326456
‘Katie <ktschr11@gmail.com>
2022-01-04 17:08:09 -0800

    snapshot test passing

--- src/components/GuestBook/Guestbook.test.jsx
+++ src/components/GuestBook/Guestbook.test.jsx
@@ -1,25 +1,31 @@
 import { render, screen, waitFor } from "@testing-library/react";
-import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
+import { EntryProvider } from "../../context/EntryContext.jsx";
+import { UserProvider } from "../../context/UserContext.jsx";
 import Guestbook from "./Guestbook.jsx";
 
-const mockLogout = jest.fn();
+// const mockLogout = jest.fn();
 
-jest.mock('../../hooks/useAuth', () => {
-  return {
-    useAuth: function () {
-      return {
-        user: { username: 'k@k.com' },
-        logout: mockLogout,
-      }
-    }
-  }
-});
+// jest.mock('../../hooks/useAuth', () => {
+//   return {
+//     useAuth: function () {
+//       return {
+//         user: { username: 'k@k.com' },
+//         logout: mockLogout,
+//       }
+//     }
+//   }
+// });
 
 it('should render the guestbook, when a user is logged in', () => {
   const { container } = render(
     <MemoryRouter>
-      <Guestbook />
+      <UserProvider>
+        <EntryProvider>
+          <Guestbook />
+        </EntryProvider>
+      </UserProvider>
     </MemoryRouter>
   );
 
